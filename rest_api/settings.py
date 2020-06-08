c0c90af53085fcd60222fb5d8ff9ec278f47cebe
antipups <nick.kurkurin@protonmail.com>
2020-06-08 14:02:38 +0300

    Финальное начало реста

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'da0x2^=m)(y%j@*e*(80@xucxat62)99j2hiwrv+d2q@-*062)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
